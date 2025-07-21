27784b798676dfeb14e1412b3986708ff576935d
Matthew Skinner <skinner@Matthews-MacBook-Pro.local>
2025-07-21 10:17:14 -0500

    updating env var

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcloud auth application-default set-quota-project elastic-edm-dev
-gcloud config set project elastic-edm-dev
+gcloud auth application-default set-quota-project $GCP_PROJECT_ID
+gcloud config set project $GCP_PROJECT_ID
 
 bq load --source_format=PARQUET dea__stg.dbt_model_lineage ./dbt_model_lineage.parquet
 bq load --source_format=PARQUET dea__stg.dbt_column_lineage ./dbt_column_lineage.parquet
